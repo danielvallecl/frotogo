33cac4483d1b9b8eb82920f07d00ccb4b139b295
Daniel Valle <daniel.vallecl@gmail.com>
2016-08-02 17:20:35 -0400

    Refactored Logo to use Rails link_to

--- app/assets/javascripts/showpage.js
+++ app/assets/javascripts/showpage.js
@@ -1,11 +1,3 @@
-$(window).scroll(function(){
-         var sticky = $('.top-menu'),
-         scroll = $(window).scrollTop();
-        if (scroll >= 200){
-            sticky.addClass('fixed');
-            sticky.slideDown(1000);
-        }else{
-            sticky.removeClass('fixed');
-            sticky.removeAttr("style"); //slideDown adds the style="block" which needs to be removed so that next time slideDown will work
-        }
-    });
+$(document).ready(function(){
+    $("#logo").slideDown(1500);
+});
