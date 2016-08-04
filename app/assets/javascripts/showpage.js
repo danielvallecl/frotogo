13453b73f7cfd6bf4fad83b75c92a32223277ad5
Daniel Valle <daniel.vallecl@gmail.com>
2016-08-04 14:04:45 -0400

    Yelp links now open in new window

--- app/assets/javascripts/showpage.js
+++ app/assets/javascripts/showpage.js
@@ -4,6 +4,6 @@
 
 $( document ).ready(function() {
   setTimeout(function() {
-    document.getElementById('logo_div').className = 'slideDown';
-  }, 0);
+    document.getElementById("logo_div").className = 'slideDown';
+  });
 });
