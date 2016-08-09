2dff496bbbd8774d2ddffd4cfc13e4e6f9b63f07
Daniel Valle <daniel.vallecl@gmail.com>
2016-08-09 14:51:42 -0400

    Getting ready to combine both projects

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -5,3 +5,13 @@ $( document ).ready(function() {
     document.getElementById("logo_div").className = 'slideDown';
   });
 });
+
+
+var rotation = function (){
+   $("#store_image").rotate({
+    angle:0,
+    animateTo:360,
+    callback: rotation
+   });
+}
+rotation();
