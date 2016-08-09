7d3f649802f5480e6db2fa71e2fc1fe487e2d383
Daniel Valle <daniel.vallecl@gmail.com>
2016-08-09 16:52:20 -0400

    Removed fault jQuery

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -5,13 +5,3 @@ $( document ).ready(function() {
     document.getElementById("logo_div").className = 'slideDown';
   });
 });
-
-
-var rotation = function (){
-   $("#store_image").rotate({
-    angle:0,
-    animateTo:360,
-    callback: rotation
-   });
-}
-rotation();
